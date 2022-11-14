1bc918004aeb659b63478e6090b1ae3726d66f70
liamhayes <liamhayes@avaya.com>
2022-11-14 13:36:23 +0000

    first commit

---
+++ src/test/java/com/poppulo/lotteryservice/LotteryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poppulo.lotteryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
