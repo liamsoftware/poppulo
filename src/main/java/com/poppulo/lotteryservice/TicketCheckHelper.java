e67964479ac33c28630aca922575a24db2027840
LiamHayes1 <liamhayes@live.ie>
2022-11-16 12:32:22 +0000

    add ticket validation

---
+++ src/main/java/com/poppulo/lotteryservice/TicketCheckHelper.java
@@ -0,0 +1,21 @@
+package com.poppulo.lotteryservice;
+
+import java.util.List;
+
+public class TicketCheckHelper {
+
+    public static boolean isValidFormat(Ticket ticket) {
+        List<Integer[]> lines = ticket.getLines();
+        for (Integer[] aLine : lines) {
+            if (aLine.length != 3) {
+                return false;
+            }
+            for (int i = 0; i < 3; i++) {
+                if (aLine[i] < 0 || aLine[i] > 2) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/com/poppulo/lotteryservice/TicketCheckHelperTest.java
@@ -0,0 +1,89 @@
+package com.poppulo.lotteryservice;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.BDDMockito.given;
+
+public class TicketCheckHelperTest {
+
+    @Mock
+    private Ticket ticket;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void isValidFormatTest() {
+        List<Integer[]> lines = new ArrayList<>();
+        Integer[] aLine = new Integer[3];
+        aLine[0] = 0;
+        aLine[1] = 1;
+        aLine[2] = 2;
+        lines.add(aLine);
+        given(ticket.getLines()).willReturn(lines);
+        boolean actual = TicketCheckHelper.isValidFormat(ticket);
+        assertTrue(actual);
+    }
+
+    @Test
+    public void isInvalidFormatTooBigArrayTest() {
+        List<Integer[]> lines = new ArrayList<>();
+        Integer[] aLine = new Integer[4];
+        aLine[0] = 0;
+        aLine[1] = 1;
+        aLine[2] = 2;
+        aLine[3] = 1;
+        lines.add(aLine);
+        given(ticket.getLines()).willReturn(lines);
+        boolean actual = TicketCheckHelper.isValidFormat(ticket);
+        assertFalse(actual);
+    }
+
+    @Test
+    public void isInvalidFormatTooSmallArrayTest() {
+        List<Integer[]> lines = new ArrayList<>();
+        Integer[] aLine = new Integer[2];
+        aLine[0] = 0;
+        aLine[1] = 1;
+        lines.add(aLine);
+        given(ticket.getLines()).willReturn(lines);
+        boolean actual = TicketCheckHelper.isValidFormat(ticket);
+        assertFalse(actual);
+    }
+
+    @Test
+    public void isInvalidFormatValueTooBigTest() {
+        List<Integer[]> lines = new ArrayList<>();
+        Integer[] aLine = new Integer[3];
+        aLine[0] = 0;
+        aLine[1] = 1;
+        aLine[2] = 3;
+        lines.add(aLine);
+        given(ticket.getLines()).willReturn(lines);
+        boolean actual = TicketCheckHelper.isValidFormat(ticket);
+        assertFalse(actual);
+    }
+
+    @Test
+    public void isInvalidFormatValueTooSmallTest() {
+        List<Integer[]> lines = new ArrayList<>();
+        Integer[] aLine = new Integer[3];
+        aLine[0] = 0;
+        aLine[1] = -1;
+        aLine[2] = 3;
+        lines.add(aLine);
+        given(ticket.getLines()).willReturn(lines);
+        boolean actual = TicketCheckHelper.isValidFormat(ticket);
+        assertFalse(actual);
+    }
+}
